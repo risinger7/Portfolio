7d492a7d9441cd0e275602f7192677892885af51
risinger7 <viktorrisinger@gmail.com>
2024-09-12 16:27:15 +0200

    first commit

---
+++ src/(pages)/about.tsx
@@ -0,0 +1,14 @@
+const hej: string = ""
+
+const About = () => {
+  return (
+    <div>
+      <div>About page here</div>
+      <div>insert link below</div>
+      <div></div>
+    </div>
+  )
+}
+
+
+export default About
